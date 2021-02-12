a850d5195b508ba5ad42a6696591cbd42773a88e
Safia <aifasshire@gmail.com>
2021-02-12 17:32:01 +0000

    Just quiz files

--- black-codher-nodejs/excercises/excercise 2/mylogger/demoLogger.js
+++
@@ -1,8 +0,0 @@
-const { logger } = require("./modules/logger.js");
-// require('console-stamp')(console, '[HH:MM:ss.l]');
-require('log-timestamp');
-logger.info("This is your first attempt");
-logger.info("Second attempt made");
-
-logger.write("Well done you have finished");
-logger.write("You can now continue");
